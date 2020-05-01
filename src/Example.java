0f5ab05dee1f815831301f2a6cf060466262f816
mathi1905 <mathiascarlisi@hotmail.fr>
2020-05-01 15:27:03 +0200

    Base du projet

---
+++ src/Example.java
@@ -0,0 +1,27 @@
+/*
+ * INFO0062 - Object-Oriented Programming
+ * Project basis
+ *
+ * Example code to filter a WAV file using audio.jar. The filter has to be implemented first.
+ *
+ * @author: J.-F. Grailet (ULiege)
+ */
+
+import be.uliege.montefiore.oop.audio.*; // Will import: TestAudioFilter, Filter, FilterException
+
+public class Example
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            Filter df = new DummyFilter(44100 * 3); // 3seconds
+
+            TestAudioFilter.applyFilter(df, "Virtual_Insanity_1m.wav", "Filtered.wav");
+        }
+        catch(Exception e)
+        {
+            System.err.println(e.getMessage());
+        }
+    }
+}
